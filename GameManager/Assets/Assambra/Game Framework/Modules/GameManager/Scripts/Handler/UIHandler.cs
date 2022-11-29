ba45dbb629dd5547b189403f1b43aecdc37cf5d2
Philipp Leutner <philipp.leutner@assambra.com>
2022-11-29 18:00:06 +0100

    Removed unused using statements;

--- GameManager/Assets/Assambra/Game Framework/Modules/GameManager/Scripts/Handler/UIHandler.cs
+++ GameManager/Assets/Assambra/Game Framework/Modules/GameManager/Scripts/Handler/UIHandler.cs
@@ -1,8 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class UIHandler : MonoBehaviour
 {
