0ca45bd54d366077522c696dbfd23e5abe763768
Philipp Leutner <philipp.leutner@assambra.com>
2022-12-04 17:58:42 +0100

    SceneUISet, usefull functions to enable or disable UIElements; OnEnable automaticly disable all enabled UIs except wich have StartActivated enabled

--- GameManager/Assets/Assambra/Game Framework/Modules/GameManager/Scripts/ScriptableObjects/SceneUISet.cs
+++ GameManager/Assets/Assambra/Game Framework/Modules/GameManager/Scripts/ScriptableObjects/SceneUISet.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SetSceneUI", menuName = "Assambra/SetSceneUI", order = 1)]
@@ -5,4 +6,92 @@ using UnityEngine;
 public class SceneUISet : ScriptableObject
 {
     public GameObject[] UIElementPrefabs;
+
+    private void OnEnable()
+    {
+        StartWithDisabledUIElements();
+    }
+
+    public void EnableUIElement(string name)
+    {
+        bool found = false;
+        for (int i = 0; i < UIElementPrefabs.Length; i++)
+        {
+            if (UIElementPrefabs[i].name == name)
+            {
+                found = true;
+                UIElement uIElement = UIElementPrefabs[i].GetComponent<UIElement>();
+                if (!uIElement.IsActive())
+                    uIElement.ChangeActiveState();
+            }
+        }
+        if(!found)
+        { Debug.LogError("Error: Dont find any UIElement with name: " + name); }
+    }
+
+    public void EnableUIElement(int index)
+    {
+        if (index <= UIElementPrefabs.Length)
+        {
+            UIElement uIElement = UIElementPrefabs[index].GetComponent<UIElement>();
+            if (!uIElement.IsActive())
+                uIElement.ChangeActiveState();
+        }
+        else
+            Debug.LogError("Error: index: " + index + " is greater then array.Length");
+    }
+
+    public void DisableUIElement(string name)
+    {
+        bool found = false;
+        for (int i = 0; i < UIElementPrefabs.Length; i++)
+        {
+            if (UIElementPrefabs[i].name == name)
+            {
+                found = true;
+                UIElement uIElement = UIElementPrefabs[i].GetComponent<UIElement>();
+                if (uIElement.IsActive())
+                    uIElement.ChangeActiveState();
+            }
+        }
+        if (!found)
+        { Debug.LogError("Error: Dont find any UIElement with name: " + name); }
+    }
+
+    public void DisableUIElement(int index)
+    {
+        if (index <= UIElementPrefabs.Length)
+        {
+            UIElement uIElement = UIElementPrefabs[index].GetComponent<UIElement>();
+            if (uIElement.IsActive())
+                uIElement.ChangeActiveState();
+        }
+        else
+            Debug.LogError("Error: index: " + index + " is greater then array.Length");
+    }
+
+    public string UIElementName(int index)
+    {
+        string retstr = "";
+        if(index <= UIElementPrefabs.Length)
+            retstr =  UIElementPrefabs[index].name;
+        else
+            Debug.LogError("Error: index: " + index + " is greater then array.Length");
+
+        return retstr;
+    }
+
+    private void StartWithDisabledUIElements()
+    {
+        Debug.Log("All active UIElements deactivated except wich have StartActivated enabled");
+
+        for (int i = 0; i < UIElementPrefabs.Length; i++)
+        {
+            UIElement uIElement = UIElementPrefabs[i].GetComponent<UIElement>();
+            if (!uIElement.StartActivated && uIElement.IsActive())
+            {
+                uIElement.ChangeActiveState();
+            }
+        }
+    }
 }
