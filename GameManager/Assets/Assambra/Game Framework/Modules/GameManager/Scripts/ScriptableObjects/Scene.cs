eab527f93f6d329e980e6cc793fd7ae7ea71d2ef
Philipp Leutner <philipp.leutner@assambra.com>
2024-04-28 16:05:52 +0200

    Added missing namespace

--- GameManager/Assets/Assambra/Game Framework/Modules/GameManager/Scripts/ScriptableObjects/Scene.cs
+++ GameManager/Assets/Assambra/Game Framework/Modules/GameManager/Scripts/ScriptableObjects/Scene.cs
@@ -1,13 +1,15 @@
-using Assambra.GameFramework.GameManager;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Scene", menuName = "Assambra/Scene", order = 1)]
-public class Scene : ScriptableObject
+namespace Assambra.GameFramework.GameManager
 {
-    public string ScenePath;
+    [CreateAssetMenu(fileName = "Scene", menuName = "Assambra/Scene", order = 1)]
+    public class Scene : ScriptableObject
+    {
+        public string ScenePath;
 
-    public bool IsFirstScene = false;
+        public bool IsFirstScene = false;
 
-    [SerializeField] public List<SceneUISet> SceneUISets = new List<SceneUISet>();
+        [SerializeField] public List<SceneUISet> SceneUISets = new List<SceneUISet>();
+    }
 }
